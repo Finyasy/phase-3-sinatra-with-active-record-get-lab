1d7d2a24e5c61d70341e898aea187a9984b2a72e
Bryan Bosire <bryanbosire@gmail.com>
2022-08-30 16:31:14 +0300

    commit tree 8229c7bf793e905563032d978538a780a23ea038

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,30 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   # add routes
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
 
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    # returns a single bakery as JSON with its baked goods nested
+    bakery.to_json(include: :baked_goods)
+  end
+
+  # GET /baked_goods/by_price
+  # returns an array of baked goods as JSON, sorted by price in descending order
+  get '/baked_goods/by_price' do
+    baked_goods = BakedGood.all.sort_by { |baked_good| baked_good.price }.reverse
+    baked_goods.to_json
+  end
+
+  # GET /baked_goods/most_expensive
+  # returns the single most expensive baked good as JSON
+  get '/baked_goods/most_expensive' do
+    baked_good = BakedGood.all.sort_by { |baked_good| baked_good.price }.last
+    baked_good.to_json
+  end
+  
 end
